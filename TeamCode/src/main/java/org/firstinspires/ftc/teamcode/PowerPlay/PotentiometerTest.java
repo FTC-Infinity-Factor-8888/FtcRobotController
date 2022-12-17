d6e8e4a9a2f1bb19b4783d9fac149ef629352350
Helen Saunders <helenelizabethsaunders@gmail.com>
2022-12-17 13:12:47 -0500

    Fixed potentiometer test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay/PotentiometerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay/PotentiometerTest.java
@@ -3,39 +3,53 @@ package org.firstinspires.ftc.teamcode.PowerPlay;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp
 public class PotentiometerTest extends LinearOpMode {
-    // Define variables for our potentiometer and motor
-    AnalogInput potentiometer;
-    DcMotor motor;
+    PowerPlayRobot r2;
 
-    // Define variable for the current voltage
-    double currentVoltage;
-
+    //This function is executed when this Op Mode is selected from the Driver Station.
     @Override
     public void runOpMode() {
-        // Get the potentiometer and motor from hardwareMap
-        potentiometer = hardwareMap.get(AnalogInput.class, "Potentiometer");
-        motor = hardwareMap.get(DcMotor.class, "Motor");
+
+        // Declaring the former values of the buttons, so we can tell if they changed.
+        // Note: Currently no such variables
+
+        // r2 has arrived.
+        r2 = new PowerPlayRobot(this);
+        r2.initHardware();        // Get the potentiometer and motor from hardwareMap
+
+        AnalogInput potentiometer;
+        CRServo motor;
+
+        // Define variable for the current voltage
+        double currentVoltage;
 
         // Loop while the Op Mode is running
         waitForStart();
         while (opModeIsActive()) {
-            // Update currentVoltage from the potentiometer
-            currentVoltage = potentiometer.getVoltage();
+            // Get the potentiometer and motor from hardwareMap
+            potentiometer = hardwareMap.get(AnalogInput.class, "Potentiometer");
+            motor = hardwareMap.get(CRServo.class, "liftMotor");
 
-            // Turn the motor on or off based on the potentiometer’s position
-            if (currentVoltage < 1.65) {
-                motor.setPower(0);
-            } else {
-                motor.setPower(0.3);
+            // Loop while the Op Mode is running
+            waitForStart();
+            while (opModeIsActive()) {
+                // Update currentVoltage from the potentiometer
+                currentVoltage = potentiometer.getVoltage();
+
+                // Turn the motor on or off based on the potentiometer’s position
+                if (currentVoltage < 1.65) {
+                    motor.setPower(0);
+                } else {
+                    motor.setPower(0.3);
+                }
+
+                // Show the potentiometer’s voltage in telemetry
+                telemetry.addData("Potentiometer voltage", currentVoltage);
+                telemetry.update();
             }
-
-            // Show the potentiometer’s voltage in telemetry
-            telemetry.addData("Potentiometer voltage", currentVoltage);
-            telemetry.update();
         }
     }
 }
